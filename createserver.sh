289377a74dc11652e21ff46a0759a4c987b403b5
Ganesh <ganeshvelugula28@gmail.com>
2023-03-08 20:35:14 +0530

    create sh update

--- createserver.sh
+++ createserver.sh
@@ -48,10 +48,15 @@ SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${SG_NA
 
 for i in catalogue cart user shipping payment frontend mongodb mysql rabbitmq redis dispatch; do
 
-  $(aws ec2 run-instances --image-id "${AMI_ID}" --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${i}}, {Key=Monitor,Value=yes}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${i}}]" --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" --security-group-ids "${SGID}")
+  PRIVATE_IP=$(aws ec2 run-instances \
+        --image-id ${AMI_ID} \
+        --instance-type t3.micro \
+        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}, {Key=Monitor,Value=yes}]" "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"  \
+        --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
+        --security-group-ids ${SGID} \
+        | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
-  IPaddress= $(aws ec2 describe-instances --filters "Name=tag:name,Values=${i}" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
 
-  aws ssm put-parameter --name ${i} --value "${IPaddress}" --type "SecureString" --region "us-east-1" --overwrite
+  aws ssm put-parameter --name ${i} --value "${PRIVATE_IP}" --type "SecureString" --region "us-east-1" --overwrite
 
 done
